aebdb979f2893474f640a132c9733f970ad8c1a9
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-06-25 11:38:28 +0800

    modified dish model

--- lib/models/dish.dart
+++ lib/models/dish.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Dish {
   final String dishName;
   final String imageFile;
+  final List<String> ingredients;
+  final List<String> cookingSteps;
 
-  const Dish({required this.dishName, required this.imageFile});
+  const Dish({required this.dishName, required this.imageFile, required this.ingredients, required this.cookingSteps});
 }
